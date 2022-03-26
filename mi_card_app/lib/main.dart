e6ed3064581e8a14d806c94de4ff680de621127c
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-03-26 13:44:51 +0500

    Lecture 3
    Understanding layout

--- mi_card_app/lib/main.dart
+++ mi_card_app/lib/main.dart
@@ -2,11 +2,44 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 100.0,
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                height: 100.0,
+                color: Colors.orange,
+                child: Text('Container 2'),
+              ),
+              Container(
+                height: 100.0,
+                color: Colors.red,
+                child: Text('Container 3'),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
